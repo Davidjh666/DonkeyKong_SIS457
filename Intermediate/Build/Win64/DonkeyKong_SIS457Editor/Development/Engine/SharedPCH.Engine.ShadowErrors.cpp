83526a44e04d93da0f666ffe8a845767ed53563e
davidjh666 <herrera.davidjesus@usfx.bo>
2024-08-18 22:22:25 -0400

    Agregar archivos de proyecto.

---
+++ Intermediate/Build/Win64/DonkeyKong_SIS457Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.39.33523
+#include "C:/Users/lenovo/Documents/Unreal Projects/DonkeyKong_SIS457/Intermediate/Build/Win64/DonkeyKong_SIS457Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
